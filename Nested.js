7c21ad31d985f001534237aaccf94322af3a9fc0
mitalisixs <mitali.sixs@gmail.com>
2020-02-07 14:05:44 +0530

    firsrt commit

---
+++ Nested.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class Nested extends React.Component{
+	render(){
+		return(
+			<div>
+				<h1>Header</h1>
+				<h2>Content</h2>
+				<p>THis is content!</p>
+			</div>
+		);
+	}
+}
+export default Nested
